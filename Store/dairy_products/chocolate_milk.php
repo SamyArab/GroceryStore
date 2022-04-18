b746733909121d0645e25fb9fe07bd354eab3244
nickuniverse91 <npiperni@hotmail.ca>
2022-04-17 20:43:01 -0400

    changed css to CSS in broken files

--- Store/dairy_products/chocolate_milk.php
+++ Store/dairy_products/chocolate_milk.php
@@ -9,7 +9,7 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/styles.css" />
+    <link rel="stylesheet" href="/CSS/styles.css" />
     <script type = "text/javascript" src = "/Store/description.js" defer></script>
     <script type="text/javascript" src="/Store/item_properties.js" defer></script>
     <title>Chocolate Milk</title>
--- Store/fruits_description/cauliflower.php
+++ Store/fruits_description/cauliflower.php
@@ -7,7 +7,7 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/styles.css"/>
+    <link rel="stylesheet" href="/CSS/styles.css"/>
     <script type = "text/javascript" src = "/Store/description.js" defer></script>
     <script type="text/javascript" src="/Store/item_properties.js" defer></script>
     <title>Cauliflower</title>
